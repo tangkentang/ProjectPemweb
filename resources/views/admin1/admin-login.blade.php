9bfbc8e4ff84075c6cf2db36edc5f5668e8bc3b4
nando <hernandoatha@student.ub.ac.id>
2024-05-30 22:55:10 +0700

    first commit

--- resources/views/admin1/admin-login.blade.php
+++ resources/views/admin1/admin-login.blade.php
@@ -63,7 +63,8 @@
 <body>
     <div class="login-container">
         <h1>Login</h1>
-        <form action="login.php" method="post">
+        <form action="admin1" method="post">
+            @csrf
             <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="password" placeholder="Password" required>
             <input type="submit" value="Login">
